ac868f6936ac4c52daf41c9a9c5a28534f07c4ed
Jacob Strelnikov <mrrocketleage@gmail.com>
2025-10-28 12:11:58 +1100

    Tried to add shop, currently shop is not responsive

--- cursed_clicker.py
+++ cursed_clicker.py
@@ -11,7 +11,29 @@ save_file = "save.json"
 save = {}
 def initsave():
     save["cookies"] = 0
-    save["cps"] = 1
+    save["store"] = {}
+    save["store"]["clickers"] = 0
+    with open("log.txt", "w") as f:
+        f.write(str(type(save["store"])))
+        for k,v in save["store"].items():
+            f.write(f"{k},{v}\n")
+def store(stdscr):
+    while True:
+        stdscr.clear()
+        stdscr.addstr(0,0,"Welcome to the store!")
+        stdscr.addstr(1,0, "Clickers add 0.5 to your cps (cookies per seconds)")
+        stdscr.addstr(2,0, "Type 0 to exit the store.")
+        line_at = 3
+        line_started = 3
+        for k,v in save["store"].items():
+            stdscr.addstr(line_started, 0, f"{line_at - line_started + 1}:{k} {v}")
+            line_at += 1
+        stdscr.refresh()
+        nput = stdscr.getkey()
+        if nput == 1:
+            save["store"]["clickers"] += 1
+        if nput == 0:
+            break
 
 if not os.path.exists(save_file):
     # Create the save file
@@ -22,7 +44,8 @@ with open(save_file, "r") as f:
     save = json.load(f)
 
 def addcookie():
-    save["cookies"] += save["cps"]
+    save["cookies"] += save["store"]["clickers"] * 0.5
+    save["cookies"] += 1
 
 
 def main(stdscr):
@@ -31,6 +54,7 @@ def main(stdscr):
         # Print amount of cookies on top line
             stdscr.clear()
             stdscr.addstr(0,10,f"You have {save['cookies']} cookies.") 
+            stdscr.addstr(1,10, "Type 1 for the shop")
             stdscr.refresh()
             nput = stdscr.getkey()
             # Check what to do
@@ -38,6 +62,9 @@ def main(stdscr):
             # If \n, add cookie
             if nput == '\n':
                 addcookie()
+            # If 1, go to shop
+            if nput == "1":
+                store(stdscr)
         except KeyboardInterrupt:
             save_to_json()
             break
